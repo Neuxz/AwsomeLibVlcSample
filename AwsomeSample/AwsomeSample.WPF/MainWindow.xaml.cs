07deaa825e8815066c4baf5cf3f396036a34a84c
Neuxz <Benedikt.oleksik@hotmail.de>
2018-10-24 10:31:18 +0200

    Initial Commit

---
+++ AwsomeSample/AwsomeSample.WPF/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿        using LibVLCSharp.Forms.Platforms.WPF;
+        using LibVLCSharp.Forms.Shared;
+        using System.Collections.Generic;
+        using System.Reflection;
+        using Xamarin.Forms;
+        using Xamarin.Forms.Platform.WPF;
+
+        namespace AwsomeSample.WPF
+        {
+            /// <summary>
+            /// Interaction logic for MainWindow.xaml
+            /// </summary>
+            public partial class MainWindow : FormsApplicationPage
+            {
+                public MainWindow()
+                {
+                    InitializeComponent();
+                    InitDependencies();
+                    Forms.Init();
+                    LoadApplication(new AwsomeSample.App());
+                }
+                void InitDependencies()
+                {
+                    var init = new List<Assembly>
+                    {
+                        typeof(VideoView).Assembly,
+                        typeof(VideoViewRenderer).Assembly
+                    };
+                }
+            }
+        }
---
+++ AwsomeSample/AwsomeSample/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibVLCSharp.Shared;
+using Xamarin.Forms;
+
+namespace AwsomeSample
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            //So here we go
+            //It runns trough the Constructor
+            //But OnAppearing() is never reached.
+            System.Diagnostics.Debugger.Break();
+            InitializeComponent();
+
+
+        }
+        protected override void OnAppearing()
+        {
+            System.Diagnostics.Debugger.Break();
+
+            base.OnAppearing();
+
+            videoView.MediaPlayer.EncounteredError += (s, e) =>
+            {
+                System.Diagnostics.Debugger.Break();
+            };
+            videoView.MediaPlayer.Stopped += (s, e) =>
+            {
+                System.Diagnostics.Debugger.Break();
+            };
+            videoView.MediaPlayer.Buffering += (s, e) =>
+            {
+                System.Diagnostics.Debugger.Break();
+            };
+            videoView.MediaPlayer.Playing += (s, e) =>
+            {
+                System.Diagnostics.Debugger.Break();
+            };
+
+            videoView.MediaPlayer.Play(new Media(videoView.LibVLC,
+                "http://www.quirksmode.org/html5/videos/big_buck_bunny.mp4", Media.FromType.FromLocation));
+        }
+    }
+}
